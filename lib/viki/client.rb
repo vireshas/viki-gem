fef158716faeb5a2bcf796ef523418b76136224a
Cedric Chin <pair+ced@viki.com>
2012-06-18 12:01:26 +0800

    Refactored client functions into separate files.

--- lib/viki/client.rb
+++ lib/viki/client.rb
@@ -5,6 +5,7 @@ require 'viki/series'
 
 module Viki
   class Client
+    Dir[File.expand_path('../client/*.rb', __FILE__)].each { |f| require f }
 
     def initialize(client_id, client_secret)
       @client_id = client_id
@@ -15,44 +16,8 @@ module Viki
     attr_reader :access_token
 
     include Viki::Request
-
-    def movies(params = {})
-      # hack until API is fixed, should return API error message
-      raise Viki::Error, "A watchable_in parameter is required when using the platform parameter" if params[:platform] && !params[:watchable_in]
-      response = request("movies", params)
-      movie_list = []
-      response["response"].each { |movie| movie_list << Movie.new(movie) }
-      movie_list
-    end
-
-    def movie(id, params = {})
-      Movie.new(request("movies/#{id}", params))
-    end
-
-    def movie_subtitles(id, lang)
-      request("movies/#{id}/subtitles/#{lang}")
-    end
-
-    def movie_hardsubs(id)
-      request("movies/#{id}/hardsubs")
-    end
-
-    def series(id = nil, params={})
-      params = id if id.is_a?(Hash)
-
-      # hack until API is fixed, should return API error message
-      raise Viki::Error, "A watchable_in parameter is required when using the platform parameter" if params[:platform] && !params[:watchable_in]
-
-      if id.is_a?(Integer)
-        Series.new(request("series/#{id}", params))
-      else
-        response = request("series", params)
-        series_list = []
-        response["response"].each { |movie| series_list << Series.new(movie) }
-        series_list
-      end
-    end
-
+    include Viki::Client::Movies
+    include Viki::Client::Series
 
   end
 end
---
+++ lib/viki/client/series.rb
@@ -0,0 +1,21 @@
+module Viki
+  class Client
+    module Series
+      def series(id = nil, params={ })
+        params = id if id.is_a?(Hash)
+
+        # hack until API is fixed, should return API error message
+        raise Viki::Error, "A watchable_in parameter is required when using the platform parameter" if params[:platform] && !params[:watchable_in]
+
+        if id.is_a?(Integer)
+          Viki::Series.new(request("series/#{id}", params))
+        else
+          response = request("series", params)
+          series_list = []
+          response["response"].each { |movie| series_list << Viki::Series.new(movie) }
+          series_list
+        end
+      end
+    end
+  end
+end
